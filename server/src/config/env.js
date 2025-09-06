e8e4b80234028fe5adfbd7feff1b66da23b559c1
AYUSH ANCHAL <abhardwaj8507@gmail.com>
2025-09-07 01:44:50 +0530

    Solving Cors Error

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,3 +1,3 @@
 export const isProd = process.env.NODE_ENV === 'production';
-export const clientUrl = process.env.CLIENT_URL || 'http://localhost:5173';
+export const clientUrl = process.env.CLIENT_URL || 'fullstack-management-system-task-ma-one.vercel.app';
 export const cookieSecure = String(process.env.COOKIE_SECURE) === 'true';
