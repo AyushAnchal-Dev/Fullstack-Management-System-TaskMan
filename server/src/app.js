8fbfa787f97aeb4d60fd75c1f4c59c1e0727ebb9
AYUSH ANCHAL <abhardwaj8507@gmail.com>
2025-09-18 22:50:50 +0530

    Solving cors error

--- server/src/app.js
+++ server/src/app.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [clientUrl],
+    origin: clientUrl,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS']
   })
 );
 
